cf4f832a7e87ddfa0811bba9d3d31626ac27ab67
Shaw Malcolm <shawmalcolm@protonmail.com>
2021-02-10 21:02:17 +0000

    capture input

--- Program.cs
+++ Program.cs
@@ -6,7 +6,9 @@ namespace learning_c_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hey Dale. How was the Black Lodge?");
+            string input = Console.ReadLine();
+            Console.WriteLine($"Confirmed. It was {input}.");
         }
     }
 }
